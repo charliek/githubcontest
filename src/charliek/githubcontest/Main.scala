ae34aeb400da8610ead40c0bb9f83d987a9802b2
unknown <CharlieK@.(none)>
2009-08-07 02:30:35 -0500

    close the output file handle

--- src/charliek/githubcontest/Main.scala
+++ src/charliek/githubcontest/Main.scala
@@ -16,14 +16,18 @@ object Main {
       val cols = line.trim.split(":")
       val (user, watched) = ( cols(0), cols(1) )
       pop(watched) = pop(watched) + 1
-
     })
     val top10 = pop.toList.sort{(a,b) => a._2 > b._2}.slice(0, 10).map(_._1)
     pop = null
-    val w = new FileWriter(out)
-    Source.fromFile(guess).getLines.foreach(line => {
-      w.write(line.trim + ":" + top10.mkString(",") + "\n")
-    })
+    var w : FileWriter = null
+    try {
+      w = new FileWriter(out)
+      Source.fromFile(guess).getLines.foreach(line => {
+        w.write(line.trim + ":" + top10.mkString(",") + "\n")
+      })
+    } finally {
+      w.close()
+    }
   }
 }
 
